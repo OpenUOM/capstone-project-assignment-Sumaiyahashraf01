0601433a192271282ab02c66f38250b1d2402474
Sumaiyahashraf01 <132247511+Sumaiyahashraf01@users.noreply.github.com>
2023-05-08 09:32:22 +0530

    database function changes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
-const express = require ("express");
-
+const express = require("express");
+const bodyParser = require("body-parser")
 const {
+  initializeDatabase,
   readTeachers,
-  readStudents,
-  addStudent,
-  addTeacher,
-  deleteTeacher,
-  deleteStudent,
-  readStudentInfo,
   readTeacherInfo,
-  updateStudent,
+  addTeacher,
   updateTeacher,
-  dbinitialize
-} = require ("./database.js");
+  deleteTeacher,
+  addStudent,
+  readStudents,
+  readStudentInfo,
+  updateStudent,
+  deleteStudent
+} = require("./database.js");
 
 const app = express();
-const bodyParser = require  ("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -58,10 +57,7 @@ app.post("/addTeacher", async function (req, res) {
 
 app.post("/editTeacher", async function (req, res) {
   let reqBody = req.body;
-  console.log(
-    "Request received to update teacher. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await updateTeacher(reqBody.name,reqBody.age,reqBody.id);
+  let data = await updateTeacher(reqBody.name, reqBody.age, reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
@@ -77,8 +73,6 @@ app.post("/deleteTeacher", async function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
-
-// ============== Student Related endpoints ==============
 
 app.get("/listStudents", async function (req, res) {
   console.log("Request received to list students");
@@ -129,7 +123,7 @@ app.post("/editStudent", async function (req, res) {
   console.log(
     "Request received to update Student. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
+  let data = await updateStudent(reqBody.name, reqBody.age, reqBody.hometown, reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
